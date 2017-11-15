043b12e489fce89e9c5b710b39e2a581550f7408
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 06:45:46 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count === 1
+    array[0]
+  elsif array.length === 2
+    last_element = array.pop
+    (array.join(", ")) + " and " + last_element
+  elsif array.length > 1
+    last_element = array.pop
+    (array.join(", ")) + ", and " + last_element
+  end
 end
