b716cecc26ad77acefb6aa3a40ae450c47a28ffb
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 06:27:54 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1
-    array[0]
+    puts array[0]
   elsif array.length > 1
     last = array.pop
-    string = (array.join(", ")) + "and" last
+    puts (array.join(", ")) + "and" last
   end
 end
