6559d7e7fa6a92563808dc9b2e0d59e92f9448fa
Remmy Clay <remmy.lauren@gmail.com>
2017-11-15 06:31:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-#  if array.length === 1
-#    puts array.join()
-  if array.length > 1
-    last = array.pop
-    puts (array.join(", ")) + "and" + last
+  if array.count === 1
+    puts array.join()
+#  elsif array.length > 1
+#    last = array.pop
+#    puts (array.join(", ")) + "and" + last
 
   end
 end
